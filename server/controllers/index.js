f5d5f622824b951a11f6a88cc86914791b2ec0c0
unknown <sujithkb24@gmail.com>
2025-11-02 19:54:21 +0530

    Initial commit: Add client and server folders

---
+++ server/controllers/index.js
@@ -0,0 +1,15 @@
+// Controller example
+// Place your controller functions here
+
+// Example controller:
+// export const getUsers = async (req, res, next) => {
+//   try {
+//     // Your logic here
+//     res.json({ success: true, data: [] });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+export default {};
+
---
+++ server/services/index.js
@@ -0,0 +1,19 @@
+// Service layer for business logic
+// Services handle complex business operations
+
+// Example service:
+// export const userService = {
+//   async createUser(userData) {
+//     // Business logic here
+//     // Validate data, perform operations, etc.
+//     return user;
+//   },
+//   
+//   async getUserById(id) {
+//     // Fetch user logic
+//     return user;
+//   }
+// };
+
+export default {};
+
